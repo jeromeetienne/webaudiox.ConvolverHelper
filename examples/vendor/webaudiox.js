6c58de7cb3fb56c6790edaf9d3abb10e3e260bf6
Jerome Etienne <jerome.etienne@gmail.com>
2014-01-08 12:31:02 +0100

    switch to unmodified webaudiox.js

--- examples/vendor/webaudiox.js
+++ examples/vendor/webaudiox.js
@@ -170,6 +170,23 @@ WebAudiox.ByteToNormalizedFloat32Array	= function(srcArray, dstArray){
 		dstArray[i]	= sum/(last-first+1);
 	}
 }
+// @namespace defined WebAudiox name space
+var WebAudiox	= WebAudiox	|| {}
+
+/**
+ * Helper to create a Convolver
+ * @param  {AudioContext} context  the webaudio api context
+ * @param  {String} url     the url of the impulse file to load
+ * @return {[type]} the just built convolver
+ */
+WebAudiox.Convolver	= function(context, url){
+	// Create Convolver
+	var convolver = context.createConvolver();
+	WebAudiox.loadBuffer(context, url, function(buffer) {	
+			convolver.buffer = buffer;
+	});
+	return convolver;
+};
 var WebAudiox	= WebAudiox	|| {}
 
 /**
@@ -419,61 +436,3 @@ WebAudiox.PannerObject3DUpdater	= function(panner, object3d){
 	}
 }
 
-// @namespace defined WebAudiox name space
-var WebAudiox = WebAudiox || {};
-/**
- * Helper to create different convolution reverbs
- * 
- * @param  {AudioContext} context   the WebAudio API context
- * @param  {[type]} convolver   the convolver node to update
- */
-WebAudiox.ConvolverHelper = function(context,convolver){
-	
-	this.baseUrl = '../';
-	
-	this.makeTelephoneFilter = function(){
-		WebAudiox.loadBuffer(context, '../impulses/filter-telephone.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};
-	this.makeSmallRoom = function(){
-		WebAudiox.loadBuffer(context, '../impulses/cinema-hallway.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};
-	this.makeMediumRoom = function(){
-		WebAudiox.loadBuffer(context, '../impulses/medium-room2.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};	
-	this.makeLargeRoom = function(){
-		WebAudiox.loadBuffer(context, '../impulses/s2_r4_bd.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};		
-	this.makeHugeWarehouse = function(){
-		WebAudiox.loadBuffer(context, '../impulses/diffusor2.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};			
-	this.makeMuffledSound = function(){
-		WebAudiox.loadBuffer(context, '../impulses/muffler.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};		
-	this.makeSpringBack = function(){
-		WebAudiox.loadBuffer(context, '../impulses/feedback-spring.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};		
-	this.makeStereoSpreader = function(){
-		WebAudiox.loadBuffer(context, '../impulses/spreader55-75ms.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};		
-	this.makeBigEcho = function(){
-		WebAudiox.loadBuffer(context, '../impulses/echo-chamber.wav', function(buffer) {
-			convolver.buffer = buffer;
-		});
-	};		
-};
